fae5129116dc476d25db128c2eb7f30e4a4d9983
Alex <alex.podikoglou@gmail.com>
2025-06-14 01:26:50 +0300

    syntax highlighting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+import { createHighlighter } from "shiki";
+
 export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets/css/*');
   eleventyConfig.addPassthroughCopy('assets/type/*');
@@ -10,5 +12,38 @@ export default async function(eleventyConfig) {
       .filter(item => !item.data.draft);
   });
 
+  /* https://claas.dev/posts/shiki-with-eleventy/ */
+  eleventyConfig.addPlugin(
+    shikiPlugin,
+    {
+      theme: "github-dark-high-contrast",
+      themes: ["github-dark-high-contrast"],
+
+      langs: [
+        "bash",
+        "html",
+        "toml",
+        "rust",
+        "kotlin",
+        "js",
+        "ts",
+        "c",
+      ],
+    }
+  );
+
 };
 
+async function shikiPlugin(configuration, options) {
+  const highlighter = await createHighlighter(options);
+  configuration.amendLibrary("md", (library) => {
+    library.set({
+      highlight: (code, language) => {
+        return highlighter.codeToHtml(code, {
+          lang: language,
+          theme: options.theme,
+        });
+      },
+    });
+  });
+};
